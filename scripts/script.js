26f4377624b4acb915e9da7f1a924d3c8a2d3bed
Elena Sviridova <valeryk13@yandex.ru>
2021-01-28 20:23:47 +0300

    second review

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,8 @@ let popupCloseIcon = formPopupContainer.querySelector('.popup__close-icon');
 let profileInfo = document.querySelector('.profile__info');
 let profileEditButton = profileInfo.querySelector('.profile__edit-button');
 let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__input_name');
-let jobInput = formElement.querySelector('.popup__input_about-yourself');
+let nameInput = formElement.querySelector('.popup__input_text_name');
+let jobInput = formElement.querySelector('.popup__input_text_about-yourself');
 let profileTitle = profileInfo.querySelector('.profile__title');
 let profileSubtitle = profileInfo.querySelector('.profile__subtitle');
 
