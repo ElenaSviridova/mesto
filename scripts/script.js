873c35fba08f8448a5a43aba7b4d2a7558364b28
Elena Sviridova <valeryk13@yandex.ru>
2021-01-28 11:48:36 +0300

    first review

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,23 @@
 let formPopup = document.querySelector('.popup');
-
 let formPopupContainer = document.querySelector('.popup__container');
-
 let popupCloseIcon = formPopupContainer.querySelector('.popup__close-icon');
-
 let profileInfo = document.querySelector('.profile__info');
-
 let profileEditButton = profileInfo.querySelector('.profile__edit-button');
-
-function showClick() {
-    let formPopup = document.querySelector('.popup');
-    formPopup.classList.add('popup_opened');
-}
-
-profileEditButton.addEventListener('click', showClick);
-
-function closeClick() {
-    let formPopup = document.querySelector('.popup');
-    formPopup.classList.remove('popup_opened');
-}
-
-popupCloseIcon.addEventListener('click', closeClick);
-
 let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__name');
-let jobInput = formElement.querySelector('.popup__about-yourself');
+let nameInput = formElement.querySelector('.popup__input_name');
+let jobInput = formElement.querySelector('.popup__input_about-yourself');
 let profileTitle = profileInfo.querySelector('.profile__title');
-
-nameInput.value = profileTitle.textContent;
-
 let profileSubtitle = profileInfo.querySelector('.profile__subtitle');
 
-jobInput.value = profileSubtitle.textContent;
+function saveClick() {
+    formPopup.classList.add('popup_opened');
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
+}
+
+function closeClick() {
+    formPopup.classList.remove('popup_opened');
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
@@ -40,4 +26,6 @@ function formSubmitHandler (evt) {
     closeClick();
 }
 
+profileEditButton.addEventListener('click', saveClick);
+popupCloseIcon.addEventListener('click', closeClick);
 formElement.addEventListener('submit', formSubmitHandler);
